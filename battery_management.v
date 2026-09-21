8c9840e6574936c246e4ebf506c2f2591e32af32
Gani1234A <154225962+Gani1234A@users.noreply.github.com>
2026-09-21 14:34:33 +0530

    Update battery_management.v

--- battery_management.v
+++ battery_management.v
@@ -1,21 +1,5 @@
 `timescale 1ns / 1ps
 
-// ================================================================
-// Project 1: Lithium-Ion Battery Modeling & SOC Estimation
-// Module   : battery_model
-// Model    : OCV(SOC) + Internal Resistance (Rint)
-// Tool     : Xilinx Vivado
-//
-// Current convention:
-//   + current = battery DISCHARGE
-//   - current = battery CHARGE
-//
-// Units:
-//   SOC        : 0 to 10000  -> 0.00% to 100.00%
-//   Current    : mA
-//   Voltage    : mV
-//   Resistance : milliohm
-// ================================================================
 
 module battery_model #(
     parameter integer R_INTERNAL_MOHM = 50
@@ -39,19 +23,11 @@ module battery_model #(
     output reg [15:0] battery_voltage_mv
 );
 
-    // ------------------------------------------------------------
-    // Internal signals
-    // ------------------------------------------------------------
-
     reg signed [31:0] voltage_drop_mv;
     reg signed [31:0] calculated_voltage_mv;
 
     // Product of current and resistance
     reg signed [31:0] current_resistance_product;
-
-    // ------------------------------------------------------------
-    // OCV Lookup Table
-    //
     // Approximate Li-ion cell OCV curve
     //
     // SOC        OCV
@@ -68,8 +44,7 @@ module battery_model #(
     // 0%         3.00 V
     //
     // Voltage is represented in mV.
-    // ------------------------------------------------------------
-
+ 
     always @(*) begin
 
         if (soc >= 14'd10000)
@@ -106,8 +81,6 @@ module battery_model #(
             ocv_mv = 16'd3000;
 
     end
-
-    // ------------------------------------------------------------
     // Battery voltage calculation
     //
     // Vbattery = OCV - I * R
@@ -131,16 +104,14 @@ module battery_model #(
     //               = 50 mV
     //
     // Vbattery = 3900 - 50
-    //          = 3850 mV
-    // ------------------------------------------------------------
+    //          = 3850 ms
 
     always @(*) begin
 
-        // Current × resistance
         current_resistance_product =
             current_ma * R_INTERNAL_MOHM;
 
-        // Convert to mV
+       
         voltage_drop_mv =
             current_resistance_product / 1000;
 
@@ -148,10 +119,6 @@ module battery_model #(
         calculated_voltage_mv =
             $signed({1'b0, ocv_mv}) - voltage_drop_mv;
 
-        // --------------------------------------------------------
-        // Voltage saturation
-        // Prevent negative voltage and unrealistic values
-        // --------------------------------------------------------
 
         if (calculated_voltage_mv < 0)
             battery_voltage_mv = 16'd0;
